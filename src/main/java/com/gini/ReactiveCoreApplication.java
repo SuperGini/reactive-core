2f3908238ff66c050db64987a93333690bedb526
iordache <faraonu_ginitoru@yahoo.com>
2022-12-21 22:04:54 +0200

    initiating project

---
+++ src/main/java/com/gini/ReactiveCoreApplication.java
@@ -0,0 +1,13 @@
+package com.gini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCoreApplication.class, args);
+    }
+
+}
