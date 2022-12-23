4335c6ec6daf0d90efe72aa37146c9e1a0ed3a29
iordache <faraonu_ginitoru@yahoo.com>
2022-12-23 07:05:14 +0200

    added configuration for mongodb to add index for username field

---
+++ src/main/java/com/gini/config/MongoConfig.java
@@ -0,0 +1,30 @@
+package com.gini.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
+
+// https://stackoverflow.com/questions/47055743/spring-data-mongodb-where-to-create-an-index-programmatically-for-a-mongo-coll
+// https://www.baeldung.com/spring-data-mongodb-reactive
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+
+    @Bean
+    public ReactiveMongoTemplate reactiveMongoTemplate(MongoClient mongoClient) {
+        ReactiveMongoTemplate mongoTemplate = new ReactiveMongoTemplate(mongoClient, databaseName);
+        mongoTemplate.indexOps("customer") // collection name string or .class
+                .ensureIndex(
+                        new Index().on("username", Sort.Direction.ASC)
+                ).subscribe();
+
+        return mongoTemplate;
+    }
+}
--- src/main/java/com/gini/repository/CustomerRepository.java
+++ src/main/java/com/gini/repository/CustomerRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 
 public interface CustomerRepository extends ReactiveMongoRepository<Customer, Integer> {
-
 }
